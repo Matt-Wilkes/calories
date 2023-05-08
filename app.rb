66199022a2d2dfe454d2a2496aca59123b54a463
Matt Wilkes <matthew.wilkes90@gmail.com>
2023-05-08 18:29:12 +0100

    second commit

---
+++ app.rb
@@ -0,0 +1,13 @@
+myWeightInLb = 0.07142857142857142
+myWeightInSt = 1
+
+def convertLbToSt(lb)
+    Float(lb)/14
+end
+
+def convertStToLb(st)
+    Float(st)*14
+end
+
+
+print convertStToLb(myWeight)
